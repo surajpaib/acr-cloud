1628b2910ef0a7f5b7557b45f8ab048dd7e59901
Suraj Pai <surajballambat@gmail.com>
2016-12-29 19:47:16 +0530

    updated

---
+++ test.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+'''
+    >>> python test.py test.mp3
+'''
+
+import os, sys
+from acrcloud.recognizer import ACRCloudRecognizer
+
+if __name__ == '__main__':
+    config = {
+        'host':'XXXXXXXX',
+        'access_key':'XXXXXXXX',
+        'access_secret':'XXXXXXXX',
+        'debug':False,
+        'timeout':10 # seconds
+    }
+    
+    '''This module can recognize ACRCloud by most of audio/video file. 
+        Audio: mp3, wav, m4a, flac, aac, amr, ape, ogg ...
+        Video: mp4, mkv, wmv, flv, ts, avi ...'''
+    re = ACRCloudRecognizer(config)
+
+    #recognize by file path, and skip 0 seconds from from the beginning of sys.argv[1].
+    print re.recognize_by_file(sys.argv[1], 0)
+
+    buf = open(sys.argv[1], 'rb').read()
+    #recognize by file_audio_buffer that read from file path, and skip 0 seconds from from the beginning of sys.argv[1].
+    print re.recognize_by_filebuffer(buf, 0)
+
